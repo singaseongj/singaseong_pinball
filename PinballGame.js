68dbd01630dbe492e3ff6499cb5b840b2a594399
Singaseong <seongj1589@gmail.com>
2025-09-12 11:10:30 +0900

    Update PinballGame.js

--- PinballGame.js
+++ PinballGame.js
@@ -1635,7 +1635,7 @@ fixViewportThen: function (callback) {
   this.gameOverOverlay.add(this.nameInputSubmit);
 
 
-// --- Back to Menu button (below Play Again) ---
+// --- Back to Menu button ---
 var backBg = game.add.graphics(60, 315);
 backBg.beginFill(0x2A2A2A, 1);
 backBg.lineStyle(2, 0x6A6A6A, 1);
@@ -1660,8 +1660,23 @@ backTxt.events.onInputUp.add(function(){
   this.disableNameEntry();
   setTimeout(this.hardResetToMenu.bind(this), 50);
 }, this);
-		
+// --- Play Again button ---
+var againBg = game.add.graphics(60, 355);
+againBg.beginFill(0x2A2A2A, 1);
+againBg.lineStyle(2, 0x6A6A6A, 1);
+againBg.drawRect(0, 0, 200, 40);
+this.gameOverOverlay.add(againBg);
+againBg.inputEnabled = true;
+if (againBg.input) againBg.input.useHandCursor = true;
 
+var againTxt = game.add.bitmapText(160, 370, "ArialBlackWhite", "PLAY AGAIN", 18);
+againTxt.anchor.set(0.5);
+this.gameOverOverlay.add(againTxt);
+againTxt.inputEnabled = true;
+if (againTxt.input) againTxt.input.useHandCursor = true;
+
+againBg.events.onInputUp.add(this.playAgain, this);
+againTxt.events.onInputUp.add(this.playAgain, this);
   
 
   // initially hidden
@@ -1960,7 +1975,17 @@ restartGame: function () {
   }, this);
 },
 
-	goToMainMenu: function () {
+
+	
+	
+	playAgain: function () {
+  this.disableNameEntry();
+  if (this.gameOverOverlay) this.gameOverOverlay.visible = false;
+  this.gameOverActive = false;
+  this.gameOver = true;
+},
+
+        goToMainMenu: function () {
   // close overlays and input
   this.gameOverActive = false;
   if (this.gameOverOverlay) this.gameOverOverlay.visible = false;
@@ -1994,10 +2019,20 @@ restartGame: function () {
 			}
 
 		// CHECKING IF THE GAME IS OVER
-		if(this.gameOver==true)
-			{
-			return; // DON'T AUTO-RESTART, WAIT FOR OVERLAY
-			}
+                if(this.gameOver==true)
+                        {
+                        // RESTORING THE BALL TO THE STARTING POSITION
+                        this.ballBody.x = this.ballStart[0]*this.PTM;
+                        this.ballBody.y = this.ballStart[1]*this.PTM;
+
+                        // CLEARING THE BALL VELOCITY
+                        this.ballBody.velocity.x = 0;
+                        this.ballBody.velocity.y = 0;
+                        this.ballBody.angularVelocity = 0;
+
+                        // SETTING THAT THE GAME IS NOT OVER
+                        this.gameOver = false;
+                        }
 
 		// THE BALL SPRITE MUST ALWAYS FOLLOW THE BOX2D BALL BODY
 		this.ballSprite.position.x = this.ballBody.x * 0.10 - 6;
